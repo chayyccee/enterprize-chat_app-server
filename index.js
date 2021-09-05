108c3647c9acfcf3453043d9e7e6ae9850c5fbac
Chinedu K. Eke <ekechinedu488@gmail.com>
2021-09-05 13:00:07 +0100

    fixed signup issue

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.use('auth', authRoutes);
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
